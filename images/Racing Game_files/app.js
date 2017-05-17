301c3ebfab577cb76db62ea70235afd449ecd3e4
umichdoe <johndoe@umich.edu>
2017-05-17 14:06:04 -0700

    added images

---
+++ images/Racing Game_files/app.js
@@ -0,0 +1,17 @@
+console.log("linked js racing game file yay!1 sanity check!!!");
+// wait for the DOM to finish loading
+$(document).ready(function() {
+  // all code to manipulate the DOM
+  // goes inside this function
+
+
+
+
+
+
+
+
+
+
+
+}); //don't change this line
