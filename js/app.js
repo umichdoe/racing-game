f5c3f3bad4a6b9b6ecbfb176c6671e82dbd96afa
umichdoe <johndoe@umich.edu>
2017-05-17 14:59:42 -0700

    got both images to move across when pressing the a key or l key

--- js/app.js
+++ js/app.js
@@ -3,11 +3,17 @@ console.log("linked js racing game file yay!1 sanity check!!!");
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
+    // body...
 
+    $(document).keydown(function(keyPressed) {
+     if (keyPressed.keyCode == 65) {
+     $('#player1').animate( {left: '+=100'}, 'fast');
+    }});
 
-
-
-
+    $(document).keydown(function(keyPressed) {
+     if (keyPressed.keyCode == 76) {
+     $('#player2').animate( {left: '+=100'}, 'fast');
+    }});
 
 
 
